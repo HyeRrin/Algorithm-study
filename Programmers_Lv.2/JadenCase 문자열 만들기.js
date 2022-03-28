691b005e85dc00bad55c66712f7d96b11f072e21
Hyerin Joo <candy980426@gmail.com>
2022-03-28 18:08:39 +0900

    📝  JadenCase 문자열 만들기 풀이 추가

---
+++ Programmers_Lv.2/JadenCase 문자열 만들기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  let answer = "";
+  s = s.toLowerCase();
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === " ") {
+      answer += " ";
+    } else if (s[i - 1] === " " || i === 0) {
+      answer += s[i].toUpperCase();
+    } else {
+      answer += s[i];
+    }
+  }
+  return answer;
+}
