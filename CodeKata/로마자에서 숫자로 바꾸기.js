8021342c06d13535d90b9731f8b765bc095b55af
Hyerin Joo <candy980426@gmail.com>
2022-09-10 22:46:54 +0900

    📝 로마자에서 숫자로 바꾸기 풀이 추가

---
+++ CodeKata/로마자에서 숫자로 바꾸기.js
@@ -0,0 +1,26 @@
+function romanToNum(s) {
+  let roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let saveNum = [];
+  let sum = 0;
+  for (let i = 0; i < s.length; i++) {
+    for (num in roman) {
+      if (s[i] === num) {
+        saveNum.push(roman[num]);
+        sum += roman[num];
+      }
+    }
+    if (saveNum[i - 1] < saveNum[i]) {
+      sum -= saveNum[i - 1] * 2;
+    }
+  }
+  return sum;
+}
