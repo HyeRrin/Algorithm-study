c8d1208b88ce947d5f5a5c6e5bd70c751973c33e
Hyerin Joo <candy980426@gmail.com>
2022-12-30 19:05:38 +0900

    📝 최소직사각형 풀이 추가

---
+++ Programmers Lv.1/최소직사각형.js
@@ -0,0 +1,21 @@
+// 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/86491
+
+// 풀이
+function solution(sizes) {
+  let w = sizes.map((el) => el[0]);
+  let h = sizes.map((el) => el[1]);
+  const len = sizes.length;
+
+  for (let i = 0; i < len; i++) {
+    if (w[i] < h[i]) {
+      let tmp = w[i];
+      w[i] = h[i];
+      h[i] = tmp;
+    }
+  }
+  const maxW = Math.max(...w);
+  const maxH = Math.max(...h);
+
+  const answer = maxW * maxH;
+  return answer;
+}
