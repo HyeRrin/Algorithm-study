f60a057b7165c1240c1cc887bc6828ea42e3aa56
Hyerin Joo <candy980426@gmail.com>
2023-01-01 19:54:22 +0900

    📝 푸대 파이트 대회 문제 추가

--- Programmers Lv.1/푸드 파이트 대회.js
+++ Programmers Lv.1/푸드 파이트 대회.js
@@ -1,11 +1,13 @@
+// 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/134240
+
+// 풀이
 function solution(food) {
-    let answer = [];
-    for(let i = 1; i < food.length; i++){
-        const count = food[i]%2 ? (food[i]-1)/2: food[i]/2;
-        for(let p=0 ; p < count ;p++){
-            answer.push(i);
-        }
-        
+  let answer = [];
+  for (let i = 1; i < food.length; i++) {
+    const count = food[i] % 2 ? (food[i] - 1) / 2 : food[i] / 2;
+    for (let p = 0; p < count; p++) {
+      answer.push(i);
     }
-    return answer.join('') + 0 + answer.reverse().join('')
+  }
+  return answer.join("") + 0 + answer.reverse().join("");
 }
